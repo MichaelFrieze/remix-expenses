9fde12446da31a81263d8d35b20b927a72cd2d52
Michael Frieze <michaelfrieze@icloud.com>
2022-11-14 21:11:02 -0500

    removed splat route

--- app/routes/$.jsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@remix-run/node';
-
-export function loader({ params }) {
-  if (params['*'] === 'exp') {
-    return redirect('/expenses');
-  }
-
-  throw new Response('Not found', { status: 404 });
-}
