9e5ab7200019626c84a63fdae6f083d547ed1718
Michael Frieze <michaelfrieze@icloud.com>
2022-11-16 09:47:19 -0500

    added server-side validation

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from '@remix-run/react';
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
 import { addExpense } from '~/data/expenses.server';
+import { validateExpenseInput } from '~/data/validation.server';
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -25,6 +26,12 @@ export default function AddExpensesPage() {
 export async function action({ request }) {
   const formData = await request.formData();
   const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
 
   await addExpense(expenseData);
   return redirect('/expenses');
