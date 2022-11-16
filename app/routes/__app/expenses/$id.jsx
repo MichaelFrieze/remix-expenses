c88f37af2240166941e0109109a11a332e9135ca
Michael Frieze <michaelfrieze@icloud.com>
2022-11-16 12:32:21 -0500

    created updateExpense function and used it in an expenses route

--- app/routes/__app/expenses/$id.jsx
+++ app/routes/__app/expenses/$id.jsx
@@ -1,9 +1,12 @@
 // /expenses/<some-id> => /expenses/expense-1, /expenses/e-1
 
+import { redirect } from '@remix-run/node';
 import { useNavigate } from '@remix-run/react';
 
 import ExpenseForm from '~/components/expenses/ExpenseForm';
 import Modal from '~/components/util/Modal';
+import { updateExpense } from '~/data/expenses.server';
+import { validateExpenseInput } from '~/data/validation.server';
 // import { getExpense } from '~/data/expenses.server';
 
 export default function UpdateExpensesPage() {
@@ -27,3 +30,18 @@ export default function UpdateExpensesPage() {
 //   const expense = await getExpense(expenseId)
 //   return expense;
 // }
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect('/expenses');
+}
