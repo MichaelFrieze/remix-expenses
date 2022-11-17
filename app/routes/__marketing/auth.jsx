7237d95875da75720b67b04d40e967734c1660ca
Michael Frieze <michaelfrieze@icloud.com>
2022-11-16 20:21:18 -0500

    extracting search parameters in auth route

--- app/routes/__marketing/auth.jsx
+++ app/routes/__marketing/auth.jsx
@@ -5,6 +5,22 @@ export default function AuthPage() {
   return <AuthForm />;
 }
 
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get('mode') || 'login';
+
+  const formData = await request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  // validate user input
+
+  if (authMode === 'login') {
+    // login logic
+  } else {
+    // signup logic (create user)
+  }
+}
+
 export function links() {
   return [{ rel: 'stylesheet', href: authStyles }];
 }
